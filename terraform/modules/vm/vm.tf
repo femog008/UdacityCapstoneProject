00db55ee7832e085d3125490e32c0c45611e1821
femog008 <femog008@yahoo.co.uk>
2020-11-20 22:37:09 -0500

    Updated variables for vm module

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -14,14 +14,12 @@ resource "azurerm_network_interface" "main" {
 resource "azurerm_linux_virtual_machine" "main" {
   name                = "${var.application_type}-vm"
   location            = "${var.location}"
-  resource_group      = "${var.resource_group}"
+  resource_group_name = "${var.resource_group}"
   size                = "${var.virtual_machine_size}"
   admin_username      = "${var.admin_username}"
+  admin_password      = "${var.admin_password}"
+  disable_password_authentication = false
   network_interface_ids = ["azurerm_network_interface.main.id"]
-  admin_ssh_key {
-    username   = "${var.admin_username}"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
